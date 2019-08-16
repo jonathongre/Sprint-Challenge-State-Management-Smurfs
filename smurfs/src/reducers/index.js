6f11beefce3dadc82bf0f51f64e9896c0ceca5c7
Jonathon Green <ericg9811@gmail.com>
2019-08-16 10:39:14 -0500

    Set up reducers. Wrap App in provider.

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,10 @@
+export const initialState = {
+    test: 'Testing'
+}
+
+export const reducer =(state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
